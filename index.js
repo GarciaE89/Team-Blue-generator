e96b22b399d017e8b94c1c621e78773550511609
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-02 12:31:25 -0700

    added intern questions and set out comments for the rest of the file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Intern = require("./lib/Intern");
 const renderHTML = require("./lib/genHTML");
 
 // empty employee Array 
-let emplArr =[];
+let emplArr = [];
 
 // Questions for all employees 
 const questions = [
@@ -36,22 +36,66 @@ const questions = [
         name: "role",
         message: "what is the employee's role?"
     },
-    
+
 ]
 
 // set of questions for manager role
 managerQs = [
     {
-    type: "input",
-    name: "officeNumber",
-    message: "Please provide Manager's office number (Required)",
-    validate: officeNumber => {
-        if (officeNumber) {
-          return true;
-        } else {
-          console.log("Please enter an office number!");
-          return false;
+        type: "input",
+        name: "officeNumber",
+        message: "Please provide Manager's office number (Required)",
+        validate: officeNumber => {
+            if (officeNumber) {
+                return true;
+            } else {
+                console.log("Please enter an office number!");
+                return false;
+            }
         }
-      }
     }
 ]
+
+// Engineer Questions  
+engineerQs = [
+    {
+        type: "input",
+        name: "github",
+        message:"Please provide engineer's GitHub Username (Required)",
+        validate: github => {
+            if (github) {
+              return true;
+            } else {
+              console.log("Please enter a GitHub username!");
+              return false;
+            }
+          }
+
+    }
+]
+
+// question for intern 
+InterQs = [
+    {
+        type: "input",
+        name: "school",
+        message: "School Intern attended? (Required)",
+        validate: school => {
+            if (school) {
+              return true;
+            } else {
+              console.log("Please enter a school name!");
+              return false;
+            }
+          }
+    }
+]
+
+// initializing function 
+const init = () => {
+
+};
+
+// function to create new employee
+
+// Add a new employee function, that will stop after you select no and will render the html file 
