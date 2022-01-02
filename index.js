1c43450be075a7cade50112ca0ca5b85969eb101
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-02 14:12:17 -0700

    added new employee function

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ engineerQs = [
 ]
 
 // question for intern 
-InterQs = [
+interQs = [
     {
         type: "input",
         name: "school",
@@ -93,9 +93,68 @@ InterQs = [
 
 // initializing function 
 const init = () => {
+    if(fs.existsSync(filePath)) {
+        inquirer.prompt({
+            type: "confirm",
+            message: "Would you like overwrite your current team roster in index.html?",
+            name: "overwrite"
+        }).then(async (response) => {
+            let overwrite = response.overwrite;
+            if(await overwrite === true) {
+                console.log("please enter team information");
+                newEmployee()
+            } else if (await overwrite === false) {
+                console.log("current index.html will not be overwriten, current index file will need to be removed from the dist folder before starting a new index.html file");
+            }
+            
+        })
+    } else {
+        console.log("To create a team profile, please enter your team's information");
+        newEmployee()
+    }
 
 };
 
 // function to create new employee
+const newEmployee = async () => {
+    await inquirer.prompt(questions)
+    .then((response) => {
+        let name = response.name;
+        let id = response.id;
+        let email = response.email;
+        let role = response.role;
+        let officeNumber;
+        let github;
+        let school;
 
+        if (role === "Engineer") {
+            inquirer.prompt(engineerQs).then((response) => {
+                github = response.github;
+                let employee = new Engineer(name, id, email, github);
+                emplArr.push(employee);
+                addEmployee(emplArr);
+            });
+        }
+        else if (role === "Manager") {
+            inquirer.prompt(managerQs).then((response) => {
+                officeNumber = response.officeNumber;
+                let employee = new Manager(name, id, email, officeNumber);
+                emplArr.push(employee);
+                addEmployee(emplArr);
+            });
+        }
+        else if (role === "Intern") {
+            inquirer.prompt(interQs).then((response) => {
+                school = response.school;
+                let employee = new Intern(name, id, email, school);
+                emplArr.push(employee);
+                addEmployee(emplArr);
+            });
+        }
+        
+        });
+
+};
+
+    
 // Add a new employee function, that will stop after you select no and will render the html file 
